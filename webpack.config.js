64bc5ea519b580937364b1b261daacc2b86cecdf
Elena Sobol <tlen.chik.hi@gmail.com>
2022-05-21 22:27:29 +0300

    Выполнено домашнее задание №7 (Cookie)

--- webpack.config.js
+++ webpack.config.js
@@ -50,10 +50,22 @@ module.exports = {
         loader: 'babel-loader',
         options: { cacheDirectory: true },
       },
+      // {
+      //   test: /projects\/.+\.html/,
+      //   use: [
+      //     { loader: './scripts/html-inject-loader.js' },
+      //     {
+      //       loader: 'raw-loader',
+      //     },
+      //   ],
+      // },
       {
-        test: /projects\/.+\.html/,
+        test: /\.html/,
+        include: [path.resolve(__dirname, 'projects')],
         use: [
-          { loader: './scripts/html-inject-loader.js' },
+          {
+            loader: './scripts/html-inject-loader.js',
+          },
           {
             loader: 'raw-loader',
           },
